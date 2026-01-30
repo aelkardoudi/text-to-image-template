f0abf7da1dd698ffe7456f02ef6807d59c047721
Abdelilah <elkardoudi.abdelilah@gmail.com>
2026-01-31 00:50:58 +0100

    Update prompt for AI fetch request

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch(request, env) {
 		const inputs = {
-			prompt: "cyberpunk cat",
+			prompt: "cat women scrashed deadly",
 		};
 
 		const response = await env.AI.run(
